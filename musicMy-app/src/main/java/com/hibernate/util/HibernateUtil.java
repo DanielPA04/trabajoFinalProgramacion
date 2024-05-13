466cec34afa6ba558a18c36aeea6983ad057d92d
danid <danid@DESKTOP-UOC6DF5>
2024-05-13 11:23:27 +0200

    Commit JCalendar
    Signed-off-by: danid <danid@DESKTOP-UOC6DF5>

--- musicMy-app/src/main/java/com/hibernate/util/HibernateUtil.java
+++ musicMy-app/src/main/java/com/hibernate/util/HibernateUtil.java
@@ -21,9 +21,9 @@ public class HibernateUtil {
 
 				Properties settings = new Properties();
 				settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-				settings.put(Environment.URL, "jdbc:mysql://127.0.0.1:3307/musicMy?useSSL=false");
-				settings.put(Environment.USER, "alumno");
-				settings.put(Environment.PASS, "alumno");
+				settings.put(Environment.URL, "jdbc:mysql://127.0.0.1:3306/musicMy?useSSL=false");
+				settings.put(Environment.USER, "root");
+				settings.put(Environment.PASS, "patata");
 				settings.put(Environment.SHOW_SQL, "true");
 				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 				settings.put(Environment.HBM2DDL_AUTO, "update");
